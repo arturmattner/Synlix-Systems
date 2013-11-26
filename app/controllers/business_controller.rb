da7fd4ee42ea89f187d4d9795b342dc41528dcd0
Artur Mattner <artur.mattner@gmail.com>
2013-11-25 23:11:26 -0600

    Added GFX's to the db

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -2,6 +2,7 @@ class BusinessController < ApplicationController
   def index
     @cpus = Cpu.all
     @rams = Ram.all
+    @gfxes = Gfx.all
   end
 
   def show
@@ -9,6 +10,8 @@ class BusinessController < ApplicationController
       @product = Cpu.find(params[:id])
     elsif params[:type] == "ram"
       @product = Ram.find(params[:id])
+    elsif params[:type] == "gfx"
+      @product = Gfx.find(params[:id])
     end
   end
 
@@ -17,19 +20,22 @@ class BusinessController < ApplicationController
   end
 
   def search_results
-    if params[:cpu] == "cpu" && params[:ram] == "ram"
+    if params[:cpu] == "cpu" && params[:ram] == "ram" && params[:gfx] == "gfx"
       cpu = Cpu.where("name LIKE ?", "%#{params[:keywords]}%").order(:name)
       ram = Ram.where("name LIKE ?", "%#{params[:keywords]}%").order(:name)
-      @products = cpu + ram
+      gfx = Gfx.where("name LIKE ?", "%#{params[:keywords]}%").order(:name)
+      @products = cpu + ram + gfx
     elsif params[:ram] == "ram"
       @products = Ram.where("name LIKE ?", "%#{params[:keywords]}%").order(:name)
     elsif params[:cpu] == "cpu"
-
       @products = Cpu.where("name LIKE ?", "%#{params[:keywords]}%").order(:name)
+    elsif params[:gfx] == "gfx"
+      @products = Gfx.where("name LIKE ?", "%#{params[:keywords]}%").order(:name)
     else
       cpu = Cpu.where("name LIKE ?", "%#{params[:keywords]}%").order(:name)
       ram = Ram.where("name LIKE ?", "%#{params[:keywords]}%").order(:name)
-      @products = cpu + ram
+      gfx = Gfx.where("name LIKE ?", "%#{params[:keywords]}%").order(:name)
+      @products = cpu + ram + gfx
     end
   end
 end
