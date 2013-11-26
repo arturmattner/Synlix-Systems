82b97e75eafe42d2dcd217fd7b45c6c953419a8a
Artur Mattner <artur.mattner@gmail.com>
2013-11-25 22:07:18 -0600

    More styling

--- app/models/ram.rb
+++ app/models/ram.rb
@@ -3,9 +3,9 @@ class Ram < ActiveRecord::Base
   has_many :lineitems
   belongs_to :ramvendor
   mount_uploader :image, ImageUploader
-  attr_accessible :clocks, :image, :name, :price, :ramvendor_id, :speed, :capacity, :type
-  validates :clocks, :image, :name, :price, :ramvendor_id, :speed, :capacity, :type, :presence => true
-  validates :clocks, :price, :speed, :capacity, :numericality => true
+  attr_accessible :clocks, :image, :name, :price, :ramvendor_id, :speed, :capacity
+  validates :clocks, :image, :name, :price, :ramvendor_id, :speed, :capacity, :presence => true
+  validates :price, :speed, :capacity, :numericality => true
 
   def type
     self.class.to_s.downcase
